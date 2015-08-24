dac13745d1fac97bd34f3c24ecb4b26af71014cb
RajeevBrahma <rajeevbrahma@hotmail.com>
2015-08-24 20:22:12 +0530

    Update traffic_server.py

--- server/traffic_server.py
+++ server/traffic_server.py
@@ -4,7 +4,6 @@ import os
 import sys
 from pubnub import Pubnub
 import dis_cal
-import sys
 import math
 import time
 import bearing
@@ -18,8 +17,10 @@ g_PASA =str('167')  	# Present Approaching Signal Angle
 g_cmd = True		# Command flag
 g_flag = True       	# check flag
 g_count = int(0)	# count variable
+g_ran1 = random.randrange(1,3,1)
+g_ran2 = random.randrange(4,7,1)
 def main_function(lat,lng):
-  global g_flag,g_count,g_NAS,g_PASA,g_cmd
+  global g_flag,g_count,g_NAS,g_PASA,g_cmd,g_ran1,g_ran2
 	
 	l_list1 = ["37.786188 -122.440033","37.787237 -122.431801","37.785359 -122.424704","37.778739 -122.423349","37.776381 -122.419514","37.772811 -122.412835",
 		   "37.765782 -122.407557","37.756809 -122.406781","37.756930 -122.405238"]
@@ -33,12 +34,13 @@ def main_function(lat,lng):
 	
 	l_lat2 = (l_list1[g_NAS-1][0:9])
 	l_lng2 = (l_list1[g_NAS-1][10:21])
-	l_distance = dis_cal.dis_calc(p_lat1,p_lng1,l_lat2,l_lng2)      # calculating the distance between the vehicle and the approaching signal
-	l_bearing  = bearing.bearng(p_lat1,p_lng1,l_lat2,l_lng2)        # calculating the angle between the vehicle and the approaching signal
+	l_distance = dis_cal.dis_calc(p_lat1,p_lng1,l_lat2,l_lng2) # calculating the distance between the vehicle and the approaching signal
+	l_bearing  = bearing.bearng(p_lat1,p_lng1,l_lat2,l_lng2)   # calculating the angle between the vehicle and the approaching signal
 	if (l_bearing >180 and l_bearing <= 360):                           
 		l_bearing = str(180-l_bearing)
 	l_brng2 = str (l_bearing)
-	ran = random.randrange(1,7,1) 
+	
+	
 	if (g_NAS<=8):	
 	
 		if((g_PASA[0]) != (l_brng2[0])):                                
@@ -46,7 +48,7 @@ def main_function(lat,lng):
 				pubnub.publish(channel ='pub_channel' ,message = ("NO",g_NAS))
 				print "server sent a revert back message to %s signal \n"%(l_signal[g_NAS-1])
 				print "Ambulance crossed %s signal \n" %(l_signal[g_NAS-1])  
-				if (g_NAS<=7):
+				if (g_NAS<=6):
 					print "Ambulance is approaching %s signal next \n" %(l_signal[g_NAS])      
 				g_NAS = int(g_NAS+1)
 				g_cmd = True
@@ -55,7 +57,7 @@ def main_function(lat,lng):
 			if (g_cmd == True):
 				print "Ambulance is %d meter far from %s signal \n" %(l_distance,l_signal[g_NAS-1])
 				
-				if(g_NAS == ran):
+				if(g_NAS == g_ran1 or g_NAS == g_ran2):
 					if (g_count <= 2):
 						print "Ambulance halted, at %s it has not received the message from server\n"%(l_signal[g_NAS-1])
 						time.sleep(10)
