1e5060de4fc1c7920b613ab72b34a6da590d9e37
RajeevBrahma <rajeevbrahma@hotmail.com>
2015-08-25 23:34:29 +0530

    Update traffic_server.py

--- server/traffic_server.py
+++ server/traffic_server.py
@@ -3,7 +3,7 @@
 import os
 import sys
 from pubnub import Pubnub
-import dis_cal
+import distance_calculation
 import math
 import time
 import bearing
@@ -11,9 +11,15 @@ import random
 
 pubnub = Pubnub(publish_key="your-publish-key",       # publish and subscribe keys
 		subscribe_key="your-subscribe-key")   
-
+g_NAS = int(1)  	# Next Approaching Signal 
+g_PASA =str('167')      # Present Approaching Signal Angle
+g_cmd = True		# Command flag
+g_count = int(0)	# count variable
+g_ran1 = random.randrange(2,4,1)
+g_ran2 = random.randrange(5,7,1)
+g_flag  = True
 def main_function(lat,lng):
-  global g_flag,g_PASA,g_NAS,g_count,g_ran1,g_ran2,g_cmd
+  	global g_flag,g_PASA,g_NAS,g_count,g_ran1,g_ran2,g_cmd
 	
 	l_list1 = ["37.786188 -122.440033","37.787237 -122.431801","37.785359 -122.424704","37.778739 -122.423349","37.776381 -122.419514","37.772811 -122.412835",
 		   "37.765782 -122.407557","37.756809 -122.406781","37.756930 -122.405238"]
@@ -32,7 +38,7 @@ def main_function(lat,lng):
 	
 	l_lat2 = (l_list1[g_NAS-1][0:9])
 	l_lng2 = (l_list1[g_NAS-1][10:21])
-	l_distance = dis_cal.dis_calc(p_lat1,p_lng1,l_lat2,l_lng2) # calculating the distance between the vehicle and the approaching signal
+	l_distance = distance_calculation.dis_calc(p_lat1,p_lng1,l_lat2,l_lng2) # calculating the distance between the vehicle and the approaching signal
 	l_bearing  = bearing.bearng(p_lat1,p_lng1,l_lat2,l_lng2)   # calculating the angle between the vehicle and the approaching signal
 	if (l_bearing >180 and l_bearing <= 360):                           
 		l_bearing = str(180-l_bearing)
